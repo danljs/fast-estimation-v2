91a5535a0a11ec4bf66878a0fd76f34664365ef8
danielliu1973 <daniel.liu1973@gmail.com>
2016-08-18 05:16:41 +0800

    re-connect

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -14,26 +14,30 @@ export default (()=>{
   let websocket
   const store = createStore(rootReducer, {}, applyMiddleware(thunk))
   store.subscribe(() => {
-      const { session, last_action } = store.getState()
-      switch (last_action.type) {
-      case POST_MESSAGE:
-          return websocket.send(JSON.stringify(last_action.message))
-      default:
-          return
-      }
+    const { session, last_action } = store.getState()
+    switch (last_action.type) {
+    case POST_MESSAGE:
+      let message = JSON.stringify(last_action.message)
+      !!!websocket || websocket.readyState === websocket.CLOSED ? open(false, message) : websocket.send(message)
+    default:
+      return
+    }
   })
 
-  !!websocket && websocket.readyState === websocket.OPEN ? websocket.close() : ''
-  store.dispatch(connecting())
-  
-  websocket = new WebSocket(`ws${!!!module.hot ? 's' : ''}${'://' + window.location.hostname}:8443`)
-
-  websocket.onmessage = event => store.dispatch(receive_message(JSON.parse(event.data)))
-  websocket.onopen = () => {
-    store.dispatch(connected())
-    store.dispatch(post_message({type:'json-request',data:'initial'}))
+  let open = (first, message) => {
+    store.dispatch(connecting())
+    websocket = new WebSocket(`ws${!!!module.hot ? 's' : ''}${'://' + window.location.hostname}:8443`)
+    websocket.onmessage = event => store.dispatch(receive_message(JSON.parse(event.data)))
+    websocket.onopen = () => {
+      store.dispatch(connected())
+      first ? store.dispatch(post_message({type:'json-request',data:'initial'})) : websocket.send(message)
+    }
+    websocket.onclose = () => console.log('websocket.onclose')
+    websocket.onerror = () => console.log('websocket.onerror')
   }
-  websocket.onclose = () => console.log('websocket.onclose')
-  websocket.onerror = () => console.log('websocket.onerror')
+
+  !!websocket && websocket.readyState === websocket.OPEN ? websocket.close() : ''
+  open(true)
+
   return store
 }())
